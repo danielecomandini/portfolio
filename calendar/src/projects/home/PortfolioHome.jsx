bc7573f2cfe04a83fe90956f466aa5dffd118784
danielecomandini <daniele_comandini@hotmail.com>
2024-11-15 12:37:24 +0000

    Preparation for About Me

--- calendar/src/projects/home/PortfolioHome.jsx
+++ calendar/src/projects/home/PortfolioHome.jsx
@@ -1,5 +1,24 @@
+import Row from "../../UI/materials/grid/Row";
+import Column from "../../UI/materials/grid/Column";
+import AboutMe from "./components/about-me/AboutMe";
+
 const PortfolioHome = () => {
-  return <div>PortfolioHome</div>;
+  return (
+    <div
+      style={{
+        margin: "0 auto",
+        marginTop: "20px",
+        width: "98%",
+      }}
+    >
+      <Row>
+        <Column laptop={3}>
+          <AboutMe />
+        </Column>
+        <Column laptop={9}>RIGHT</Column>
+      </Row>
+    </div>
+  );
 };
 
 export default PortfolioHome;
---
+++ calendar/src/projects/home/components/about-me/AboutMe.jsx
@@ -0,0 +1,28 @@
+import { Card, Typography } from "@mui/material";
+import Row from "../../../../UI/materials/grid/Row";
+import Column from "../../../../UI/materials/grid/Column";
+
+const AboutMeInfo = ({label, value}) => {
+    return <Row>
+        <Column laptop={2} mobile={2}>
+            <h1>{label}</h1>
+        </Column>
+        <Column laptop={10} mobile={10}>{value}</Column>
+    </Row>
+}
+
+const AboutMe = () => {
+  return <Card>
+    <Row>
+        <Column laptop={3} mobile={3}>
+            PICTURE HERE...
+        </Column>
+        <Column laptop={9} mobile={9}>
+            <AboutMeInfo label="FIRST NAME" value="Daniele" />
+            <AboutMeInfo label="LAST NAME" value="Comandini" />
+        </Column>
+    </Row>
+  </Card>;
+};
+
+export default AboutMe;
